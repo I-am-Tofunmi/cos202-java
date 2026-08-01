8ecf6e194cf62c5870bec466acfb8d493de0341e
Tofunmi <frostycalmen@gmail.com>
2026-08-01 19:32:03 +0100

    feat: week 7 complete - queue, linkedlist, arraydeque, blockingqueue

---
+++ topic-03-java-collection/Linkedlist.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+class Linkedlist {
+    public static void main(String[] args) {
+
+        // create Linkedlist
+        LinkedList<String> animals = new LinkedList<>();
+
+        // Add elements to LinkedList
+        animals.add("Dog");
+        animals.add("Horse");
+        animals.add("Cat");
+        System.out.println("LinkedList: " + animals);
+    }
+}
